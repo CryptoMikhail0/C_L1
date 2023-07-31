2166a455b4d6824c3ab13e9b168eeeae08c3f35b
mikhail kirienko <mk19979797@yandex.ru>
2023-07-31 21:25:32 +0300

    Example005_ifElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mikhail")
+{
+    Console.WriteLine("Шабат-шалом, Mikhail");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
